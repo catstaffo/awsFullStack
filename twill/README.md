518a319c02f91c813dda35d31d27add75f402def
Catherine Stafford <catstaffo@gmail.com>
2023-01-09 21:23:00 -0500

    first commit

---
+++ twill/README.md
@@ -0,0 +1,7 @@
+further resources: 
+
+using graphql on aws
+=> https://docs.aws.amazon.com/appsync/latest/devguide/designing-a-graphql-api.html
+
+authentication
+=> https://aws-amplify.github.io/amplify-js/api/classes/authclass.html
---
+++ twill/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "twill": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "twill305db5e0": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
